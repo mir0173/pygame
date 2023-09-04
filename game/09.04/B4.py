44d5397778abd4156bb9af322bd3e96236a70c64
algorimark <105226557+algorimark@users.noreply.github.com>
2023-09-04 22:23:07 +0900

    Add files via upload

---
+++ game/09.04/B4.py
@@ -0,0 +1,11 @@
+import pygame
+class B4():
+    def __init__(self,screen,font):
+        self.screen_number = 19
+        self.screen = screen
+        self.font = font
+
+    def story(self):
+
+
+
